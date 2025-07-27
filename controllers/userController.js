67737970cee08686a72389972d849bf1d6afaf6d
Balaji-1805 <ugginabalaji2003@gmail.com>
2025-07-27 06:58:46 +0530

    Updated code in reset password controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -76,68 +76,6 @@ const getUsers=async(req,res)=>{
 
 //forgot password controller function
 
-const forgotPassword = async (req, res) => {
-    const { email } = req.body;
-    try {
-        const user = await User.findOne({ email });
-        if (!user) return res.status(404).json({ message: 'Invalid Email, Please Enter Valid Email' });
 
-        let token;
-        token = crypto.randomBytes(32).toString('hex');
-        user.resetToken = token;
-        user.resetTokenExpiry = Date.now() + 1000 * 60 * 15;
-
-        await user.save();
-
-        const resetLink = `https://notes-management-frontend-ui.vercel.app/reset-password/${token}`;
-
-        const transporter = nodemailer.createTransport({
-            service: "gmail",
-            auth: {
-                user: process.env.USER_EMAIL,
-                pass: process.env.USER_PASS
-            }
-        });
-
-        await transporter.sendMail({
-            to: user.email,
-            subject: "Password Reset Link",
-            html: `<a href="${resetLink}">Click Here to reset your Password</a>`
-        });
-
-        console.log("Reset Link Sent");
-        console.log(user.email);
-        res.json({ message: "Reset link Sent",token });
-    } 
-    catch (err) {
-        console.log("Error in Forgot Password controller:", err);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
-};
-
-//reset password
-const resetPassword=async(req,res)=>{
-    try
-    {
-        const {token}=req.params;
-        const {newPassword}=req.body;
-        const user=await User.findOne({
-            resetToken:token,
-            resetTokenExpiry:{$gt:Date.now()},//checks whether it is expired or not
-        });
-        if(!user) return res.status(400).json({error:"Invalid or Expired Token"});
-        const updatePassword=await bcrypt.hash(newPassword,10);
-        user.password=updatePassword;
-        user.resetToken=undefined;
-        user.resetTokenExpiry=undefined;
-        await user.save();
-        res.status(200).json({message:"Password reset successfully!"});
-    }
-    catch(err)
-    {
-        console.log("Error in reset password controller:",err);
-        res.status(500).json({error:"Internal Server Error"});
-    }
-}
 
 module.exports={register,login,getUsers,forgotPassword,resetPassword};
